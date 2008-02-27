4930d69155b2d311d2fefd588f7ce1f2a367766a
bsd178 <bsd178>
2008-02-27 19:32:47 +0000

    Added a generic Bag and associated tests.

---
+++ GTCommon/Bag.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GT.Common
+{
+    #region Utility Classes
+
+    public class Bag<T> : ICollection<T>
+    {
+        protected Dictionary<T, int> contents;
+        protected int totalSize;
+
+        public Bag()
+        {
+            this.contents = new Dictionary<T, int>();
+            totalSize = 0;
+        }
+
+        #region ICollection<T> Members
+
+        public void Add(T item)
+        {
+            int count;
+            if (!contents.TryGetValue(item, out count))
+            {
+                count = 0;
+            }
+            contents[item] = ++count;
+            totalSize++;
+        }
+
+        public void Clear()
+        {
+            contents.Clear();
+            totalSize = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            return contents.ContainsKey(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            foreach (T item in contents.Keys)
+            {
+                for (int count = contents[item]; count > 0; count--)
+                {
+                    array[arrayIndex++] = item;
+                }
+            }
+        }
+
+        public int Count
+        {
+            get { return totalSize; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        public bool Remove(T item)
+        {
+            int count;
+            if (!contents.TryGetValue(item, out count))
+            {
+                return false;
+            }
+            totalSize--;
+            if (count > 1)
+            {
+                contents[item] = count - 1;
+            } else {
+                contents.Remove(item);
+            }
+            return true;
+        }
+
+        #endregion
+
+        #region IEnumerable<T> Members
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return contents.Keys.GetEnumerator();
+        }
+
+        #endregion
+
+        #region IEnumerable Members
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return contents.Keys.GetEnumerator();
+        }
+
+        #endregion
+    }
+
+    #endregion
+}
---
+++ GTUnitTests/BagTests.cs
@@ -0,0 +1,83 @@
+using System;
+using NUnit.Framework;
+
+namespace GT.Common
+{
+    [TestFixture]
+    public class BagTests
+    {
+        [Test]
+        public void TestNewBag()
+        {
+            Bag<string> b = new Bag<string>();
+            Assert.AreEqual(0, b.Count);
+            Assert.IsFalse(b.IsReadOnly);
+            Assert.AreEqual(0, b.Count);
+            Assert.IsFalse(b.Remove("foo"));
+            Assert.IsFalse(b.Contains("bar"));
+            try
+            {
+                b.CopyTo(new string[0], 0);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("CopyTo() shouldn't touch parameter array if nothing to copy");
+            }
+        }
+
+        [Test]
+        public void TestAddAndRemove()
+        {
+            Bag<string> b = new Bag<string>();
+            Assert.AreEqual(0, b.Count);
+            b.Add("foo");
+            Assert.AreEqual(1, b.Count);
+            b.Add("bar");
+            Assert.AreEqual(2, b.Count);
+            b.Add("foo");
+            Assert.AreEqual(3, b.Count);
+            Assert.IsTrue(b.Contains("foo"));
+            Assert.IsTrue(b.Contains("bar"));
+            Assert.IsFalse(b.Contains("baz"));
+
+            Assert.IsTrue(b.Remove("foo"));
+            Assert.AreEqual(2, b.Count);
+            Assert.IsTrue(b.Contains("foo"));
+
+            Assert.IsTrue(b.Remove("foo"));
+            Assert.AreEqual(1, b.Count);
+            Assert.IsFalse(b.Contains("foo"));
+
+            Assert.IsFalse(b.Remove("foo"));
+            Assert.AreEqual(1, b.Count);
+            Assert.IsFalse(b.Contains("foo"));
+
+            b.Clear();
+            Assert.AreEqual(0, b.Count);
+            Assert.IsFalse(b.Contains("foo"));
+        }
+
+        [Test]
+        public void TestCopyTo()
+        {
+            Bag<string> b = new Bag<string>();
+            try { b.CopyTo(new string[0], 0); }
+            catch (Exception) { Assert.Fail("shouldn't access array unnecessarily"); }
+
+            b.Add("foo");
+                string[] values = new string[1];
+            try { b.CopyTo(values, 0); }
+            catch (Exception) { Assert.Fail("shouldn't go over array length"); }
+            Assert.AreEqual("foo", values[0]);
+        }
+
+        [Test]
+        public void TestCopyToOOBI() {
+            Bag<string> b = new Bag<string>();
+            b.Add("foo");
+            try { b.CopyTo(new string[0], 0); }
+            catch (Exception) { return; }
+            Assert.Fail(); 
+        }
+    }
+}
