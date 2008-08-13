ee0b817e5d65b1152b85c49d983f415d67be6c0b
Brian de Alwis <brian.de.alwis@usask.ca>
2008-08-13 17:37:40 -0600

    Made explicit command-line versions of the stressing client and server, from in the stress unit tests.  Useful for running the tests from different machines.

---
+++ StressorClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GT.UnitTests;
+
+namespace StressorClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Use: <program> host port");
+                return;
+            }
+            StressingClient client = new StressingClient(args[0], args[1]);
+            try
+            {
+                client.Start();
+                Console.WriteLine("Client started; press a key to stop");
+                Console.ReadKey();
+            }
+            finally
+            {
+                client.Stop();
+                client.Dispose();
+            }
+            Console.WriteLine("Client shut down");
+
+        }
+    }
+}
---
+++ StressorServer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GT.UnitTests;
+
+namespace StressorServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Use: <program> port");
+                return;
+            }
+            StressingServer server = new StressingServer(int.Parse(args[0]));
+            try
+            {
+                server.Start();
+                Console.WriteLine("Server started; press a key to stop");
+                Console.ReadKey();
+            }
+            finally
+            {
+                server.Stop();
+                server.Dispose();
+            }
+            Console.WriteLine("Server shut down");
+        }
+    }
+}
