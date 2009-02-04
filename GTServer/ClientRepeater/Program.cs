435ea54a7699a9b7fefb98cf42ae207f721b0efe
Brian de Alwis <brian.de.alwis@usask.ca>
2009-02-04 13:02:45 -0600

    Added command-line support to set a maximum packet size and the channel to use for session announcements.  Moved the ClientRepeater onto the new GetOpt.

--- GTServer/ClientRepeater/Program.cs
+++ GTServer/ClientRepeater/Program.cs
@@ -5,6 +5,8 @@ using System.IO;
 using System.Net.Sockets;
 using System.Threading;
 using System.Diagnostics;
+using Common.Logging;
+using GT.Utils;
 
 namespace GT.Net
 {
@@ -27,6 +29,13 @@ namespace GT.Net
     /// </remarks>
     public class RepeaterConfiguration : DefaultServerConfiguration
     {
+        /// <summary>
+        /// Set the maximum packet size to be configured for all transports.
+        /// If 0, then do not change the maximum packet size from the transport's
+        /// default.
+        /// </summary>
+        public uint MaximumPacketSize { get; set; }
+
         public RepeaterConfiguration(int port)
             : base(port)
         {
@@ -43,6 +52,12 @@ namespace GT.Net
         {
             return new LightweightDotNetSerializingMarshaller();
         }
+
+        public override ITransport ConfigureTransport(ITransport t)
+        {
+            if (MaximumPacketSize > 0) { t.MaximumPacketSize = MaximumPacketSize; }
+            return base.ConfigureTransport(t);
+        }
     }
 
     /// <summary>
@@ -52,44 +67,78 @@ namespace GT.Net
     /// </summary>
     public class ClientRepeater : IStartable
     {
+        public static uint DefaultPort = 9999;
+        public static byte DefaultSessionChannel = 0;
+
+        protected ILog log;
         protected ServerConfiguration config;
         protected Server server;
         protected Thread serverThread;
-        protected int sessionChangesChannel = 0;
-        protected uint verbose = 0;
 
+
+        /// <summary>
+        /// The channel for automatically broadcasting session changes to client members.  
+        /// If &lt; 0, then not sent.
+        /// </summary>
+        public int SessionChangesChannel { get; set; }
+
+        public uint Verbose { get; set; }
+        
         public Server Server { get { return server; } }
+
+        static void Usage()
+        {
+            Console.WriteLine("Use: <ClientRepeater.exe> [-v] [-m pktsize] [-s channel] [port]");
+            Console.WriteLine("  -v   be more verbose");
+            Console.WriteLine("  -s   cause session announcements to be sent on specified channel");
+            Console.WriteLine("       (use -1 to disable session announcements)");
+            Console.WriteLine("  -m   set the maximum packet size to <pktsize>");
+            Console.WriteLine("[port] defaults to {0} if not specified", DefaultPort);
+            Console.WriteLine("[channel] defaults to {0} if not specified", DefaultSessionChannel);
+        }
 
         static void Main(string[] args)
         {
-            int port = 9999;
-            int optind;
+            int port = (int)DefaultPort;
             uint verbose = 0;
+            uint maxPacketSize = 0;
+            int sessionChannel = DefaultSessionChannel;
 
-            // poor man's getopt
-            for (optind = 0; optind < args.Length && args[optind].StartsWith("-"); optind++)
+            GetOpt options = new GetOpt(args, "vm:s:");
+            try
             {
-                Debug.Assert(args[optind][0] == '-');
-                if (args[optind][1] == '-') { optind++; break; }
-                else if (args[optind][1] == 'v') { verbose++; }
-                else
+                Option opt;
+                while ((opt = options.NextOption()) != null)
                 {
-                    Console.WriteLine("Invalid option: '{0}'", args[optind]);
-                    Console.WriteLine("Use: <ClientRepeater.exe> [-v] [port]");
-                    Console.WriteLine("[port] defaults to {0} if not specified", port);
-                    return;
+                    switch (opt.Character)
+                    {
+                        case 'v':
+                            verbose++;
+                            break;
+                        case 'm':
+                            maxPacketSize = uint.Parse(opt.Argument);
+                            break;
+                        case 'b':
+                            sessionChannel = int.Parse(opt.Argument);
+                            break;
+                    }
                 }
             }
-
-            if (args.Length - optind > 1)
+            catch (GetOptException e)
             {
-                Console.WriteLine("Use: <ClientRepeater.exe> [-v] [port]");
-                Console.WriteLine("[port] defaults to {0} if not specified", port);
+                Console.WriteLine(e.ToString());
+                Usage();
                 return;
             }
-            else if (args.Length - optind == 1)
+            args = options.RemainingArguments();
+            if (args.Length > 1)
             {
-                port = Int32.Parse(args[optind]);
+                Usage();
+                return;
+            }
+            if (args.Length == 1)
+            {
+                port = Int32.Parse(args[0]);
                 if (port <= 0)
                 {
                     Console.WriteLine("error: port must be greater than 0");
@@ -98,34 +147,23 @@ namespace GT.Net
             }
 
             if (verbose > 0) { Console.WriteLine("Starting server on port {0}", port); }
-            ClientRepeater cr = new ClientRepeater(port);
+            RepeaterConfiguration config = new RepeaterConfiguration(port);
+            config.MaximumPacketSize = maxPacketSize;
+            ClientRepeater cr = new ClientRepeater(config);
+            cr.SessionChangesChannel = sessionChannel;
             cr.Verbose = verbose;
-            cr.StartListening();
+            cr.Start();
             if (verbose > 0) { Console.WriteLine("Server stopped"); }
         }
 
-        public ClientRepeater(int port) : this(new RepeaterConfiguration(port)) {}
+        public ClientRepeater(int port) : this(new RepeaterConfiguration(port)) { }
 
         public ClientRepeater(ServerConfiguration sc) {
+            log = LogManager.GetLogger(GetType());
             config = sc;
         }
 
         public bool Active { get { return server != null && server.Active; } }
-        public uint Verbose
-        {
-            get { return verbose; }
-            set { verbose = value; }
-        }
-
-        /// <summary>
-        /// The channel for automatically broadcasting session changes to client members.  
-        /// If &lt; 0, then not sent.
-        /// </summary>
-        public int SessionChangesChannel
-        {
-            get { return sessionChangesChannel; }
-            set { sessionChangesChannel = value; }
-        }
 
         /// <summary>
         /// Start an independently running client-repeater instance.
@@ -174,33 +212,44 @@ namespace GT.Net
 
         private void s_ErrorEvent(ErrorSummary es)
         {
-            if (verbose == 0 && es.Severity < Severity.Error)
-            {
-                return;
-            }
+            string message;
             if (es.Context != null)
             {
-                Console.WriteLine("{0}: {1}[{2}]: {3} [{4}: {5}]", DateTime.Now, es.Severity,
+                message = String.Format("{0}[{1}]: {2} [{3}: {4}]", es.Severity,
                     es.ErrorCode, es.Message, es.Context.GetType().Name, es.Context.Message);
             }
             else
             {
-                Console.WriteLine("{0}: {1}[{2}]: {3}", DateTime.Now, es.Severity,
+                message = String.Format("{0}[{1}]: {2}", es.Severity,
                     es.ErrorCode, es.Message);
+            }
+            switch(es.Severity)
+            {
+            case Severity.Error:
+                log.Error(message, es.Context);
+                break;
+            case Severity.Warning:
+                log.Warn(message, es.Context);
+                break;
+            case Severity.Fatal:
+                log.Fatal(message, es.Context);
+                break;
+            case Severity.Information:
+                log.Info(message, es.Context);
+                break;
             }
         }
 
         private void s_ClientsJoined(ICollection<IConnexion> list)
         {
-            if (verbose > 0)
+            if (log.IsInfoEnabled)
             {
-                DateTime now = DateTime.Now;
                 foreach(ConnexionToClient client in list)
                 {
                     client.TransportAdded += _client_TransportAdded;
                     client.TransportRemoved += _client_TransportRemoved;
 
-                    StringBuilder builder = new StringBuilder("Client ");
+                    StringBuilder builder = new StringBuilder("Client joined: ");
                     builder.Append(client.Identity);
                     builder.Append(':');
                     builder.Append(client.ClientGuid);
@@ -211,10 +260,10 @@ namespace GT.Net
                         builder.Append(t.ToString());
                         builder.Append('}');
                     }
-                    Console.WriteLine("{0}: client joined: {1}", now, builder.ToString());
+                    log.Info(builder.ToString());
                 }
             }
-            if (sessionChangesChannel < 0) { return; }
+            if (SessionChangesChannel < 0) { return; }
            
             foreach (ConnexionToClient cnx in list)
             {
@@ -224,13 +273,13 @@ namespace GT.Net
                 {
                     try
                     {
-                        c.Send(clientId, SessionAction.Joined, (byte)sessionChangesChannel,
+                        c.Send(clientId, SessionAction.Joined, (byte)SessionChangesChannel,
                             new MessageDeliveryRequirements(Reliability.Reliable, MessageAggregation.Immediate,
                                 Ordering.Unordered), null);
                     }
                     catch (GTException e)
                     {
-                        Console.WriteLine("{0}: EXCEPTION: when sending: {1}", DateTime.Now, e);
+                        log.Warn("Exception raised on send: " + e.Message, e);
                     }
                 }
             }
@@ -238,12 +287,11 @@ namespace GT.Net
 
         private void s_ClientsRemoved(ICollection<IConnexion> list)
         {
-            if (verbose > 0)
+            if (log.IsInfoEnabled)
             {
-                DateTime now = DateTime.Now;
                 foreach(ConnexionToClient client in list)
                 {
-                    StringBuilder builder = new StringBuilder("Client ");
+                    StringBuilder builder = new StringBuilder("Client left");
                     builder.Append(client.Identity);
                     builder.Append(':');
                     builder.Append(client.ClientGuid);
@@ -254,10 +302,10 @@ namespace GT.Net
                         builder.Append(t.ToString());
                         builder.Append('}');
                     }
-                    Console.WriteLine("{0}: client left: {1}", now, builder.ToString());
+                    log.Info(builder.ToString());
                 }
             }
-            if (sessionChangesChannel < 0) { return; }
+            if (SessionChangesChannel < 0) { return; }
 
             foreach (ConnexionToClient client in list)
             {
@@ -269,21 +317,21 @@ namespace GT.Net
                 {
                     client.Dispose();
                 } catch(Exception e) {
-                    Console.WriteLine("{0} EXCEPTION: when stopping Client {1}: {2}",
-                        DateTime.Now, clientId, e);
+                    log.Warn(String.Format("Exception raised when stopping client {0}: {1}",
+                        clientId, e.Message), e);
                 }
 
                 //inform others client is gone
                 foreach (ConnexionToClient c in server.Clients)
                 {
                     try {
-                        c.Send(clientId, SessionAction.Left, (byte)sessionChangesChannel,
+                        c.Send(clientId, SessionAction.Left, (byte)SessionChangesChannel,
                             new MessageDeliveryRequirements(Reliability.Reliable, MessageAggregation.Immediate,
                                 Ordering.Unordered), null);
                     }
                     catch (GTException e)
                     {
-                        Console.WriteLine("{0} EXCEPTION: when sending: {1}", DateTime.Now, e);
+                        log.Warn("Exception raised on send: " + e.Message, e);
                     }
                 }
             }
@@ -291,25 +339,25 @@ namespace GT.Net
 
         private void _client_TransportAdded(IConnexion connexion, ITransport newTransport)
         {
-            if (verbose > 0)
+            if (Verbose > 0)
             {
-                Console.WriteLine("{0} Client {1}: transport added: {2}", DateTime.Now,
-                    connexion.Identity, newTransport);
+                Console.WriteLine(String.Format("{0}: Client {1}: transport added: {2}",
+                    DateTime.Now, connexion.Identity, newTransport));
             }
         }
 
         private void _client_TransportRemoved(IConnexion connexion, ITransport newTransport)
         {
-            if (verbose > 0)
+            if (Verbose > 0)
             {
-                Console.WriteLine("{0} Client {1}: transport removed: {2}", DateTime.Now,
-                    connexion.Identity, newTransport);
+                Console.WriteLine(String.Format("{0}: Client {1}: transport removed: {2}",
+                    DateTime.Now, connexion.Identity, newTransport));
             }
         }
 
         private void s_MessageReceived(Message m, IConnexion client, ITransport transport)
         {
-            if (verbose > 1)
+            if (Verbose > 1)
             {
                 Console.WriteLine("{0}: received message: {1} from Client {2} via {3}", 
                     DateTime.Now, m, client.Identity, transport);
