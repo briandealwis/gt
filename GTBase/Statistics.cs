ac087c11f910daf8bfceb3deec5b55ad9cebbe10
Brian de Alwis <brian.de.alwis@usask.ca>
2009-04-30 19:35:20 -0600

    Preparation for GT v2.0.1:   * brought in the proper LGPL 2.1 license, added it to the installer   * added LGPL headers to all source files   * cleaned up assembly information files

--- GTBase/Statistics.cs
+++ GTBase/Statistics.cs
@@ -1,17 +1,38 @@
+//
+// GT: The Groupware Toolkit for C#
+// Copyright (C) 2006 - 2009 by the University of Saskatchewan
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later
+// version.
+// 
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+// 02110-1301  USA
+// 
+
+// This code was ported and lightly C#'d from JUNG 1.7.6's
+// edu.uci.ics.jung.statistics.StatisticalMoments
+// This could be implemented in terms of properties
+// and overloaded += methods.
+
 /*
 * Copyright (c) 2003, the JUNG Project and the Regents of the University 
 * of California
 * All rights reserved.
 *
 * This software is open-source under the BSD license; see either
-* "license.txt" or
-* http://jung.sourceforge.net/license.txt for a description.
+* "license.txt" or * http://jung.sourceforge.net/license.txt for
+* a description.
 */
-
-// This code was ported and lightly C#'d from JUNG 1.7.6's
-// edu.uci.ics.jung.statistics.StatisticalMoments
-// This could be implemented in terms of properties
-// and overloaded += methods.
 
 using System;
 namespace GT.Utils
